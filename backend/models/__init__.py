09fa1a9084c32029139f3ae17cbcc0942572bf9c
= <=>
2024-04-13 19:52:55 +0500

    Created Word model.

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User
 from .organization import Organization
 from .document import Document
 from .sense import Sense
+from .word import Word
---
+++ backend/models/word.py
@@ -0,0 +1,15 @@
+from db_init import db
+from .enums import Language
+from sqlalchemy.dialects.postgresql import ENUM
+
+
+class Word(db.Model):
+    __tablename__ = 'Word'
+
+    id = db.Column(db.String(255), primary_key=True)
+    written_form = db.Column(db.String(50), nullable=False)
+    part_of_speech = db.Column(db.String(10), nullable=False)
+    language = db.Column(ENUM(Language, create_type=False), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.id} - {self.written_form} - {self.part_of_speech}"
