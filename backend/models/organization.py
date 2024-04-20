dbde1c54c9f9541bc2696ccc7bd216e79636963d
= <=>
2024-04-20 16:32:05 +0500

    Added schemas for user.

--- backend/models/organization.py
+++ backend/models/organization.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from db_init import db
 
 
@@ -6,6 +8,7 @@ class Organization(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc).isoformat())
     owner_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
 
     def __repr__(self) -> str:
